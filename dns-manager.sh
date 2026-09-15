445a80e6a5f6a3993398e7cdf97bd955d790d2e7
PoTuStoronu222 <maximxaker1@gmail.com>
2026-09-15 15:46:56 +0300

    Update dns-manager.sh

--- dns-manager.sh
+++ dns-manager.sh
@@ -2,7 +2,7 @@
 MANAGER_PATH="/usr/bin/dns-manager"
 # ==========================================
 # ==========================================
-VERSION="2.13"
+VERSION="2.14"
 BASE_DIR="/etc/dns-manager"
 CFG_DIR="$BASE_DIR/config"
 STATE_DIR="/var/run/dns-manager"
